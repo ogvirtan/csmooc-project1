6a2efcb33301c734ed821137021419a4030cf64e
Olli Virtanen <ovirtanen96@gmail.com>
2026-07-14 16:28:43 +0300

    removed Django tutorials comments from backend for clarity

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,10 +14,6 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """
-        Return the last five published questions (not including those set to be
-        published in the future).
-        """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
@@ -27,9 +23,6 @@ class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
 
     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
@@ -41,7 +34,6 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.GET['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -49,9 +41,7 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
+
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
 
 def create_poll(request):
