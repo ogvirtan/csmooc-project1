adc25c02fbad55164e3270b257013dbf84c2cc76
Olli Virtanen <ovirtanen96@gmail.com>
2026-07-14 16:25:41 +0300

    Added sample data, tweaked admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -8,11 +8,11 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
+        (None,               {'fields': ['question_text', 'asker']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_display = ('question_text', 'asker', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
